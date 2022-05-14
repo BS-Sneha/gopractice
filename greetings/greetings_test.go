cd6d988126b2bdc5e8dd54b268e28653e0174a10
BS-Sneha <sneha.sridhar@radiantcloud.co>
2022-05-14 17:34:38 +0530

    new test file

---
+++ greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+	name := "glady"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("glady")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("glady") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
